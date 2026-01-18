9edf3bf07fe97365a7cd55532ee551e5c01d0683
Fraol <fraolm41@gmail.com>
2026-01-18 22:35:50 +0300

    fix views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library, Book  # <-- MUST include Library
 
 
