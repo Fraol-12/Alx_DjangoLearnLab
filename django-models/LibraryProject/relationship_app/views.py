4aca247e6c4785cdb5675d7c06a7b6f7b6b576cc
Fraol <fraolm41@gmail.com>
2026-01-18 22:27:07 +0300

    fix views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
-from .models import Book
+from django.views.generic import DetailView
+from .models import Library, Book  # <-- MUST include Library
+
 
 def list_books(request):
     books = Book.objects.all()  # <-- MUST be exactly this
@@ -11,3 +13,8 @@ def list_books(request):
         'books': books
     })
 
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # <-- MUST match this exactly
+    context_object_name = 'library' 
