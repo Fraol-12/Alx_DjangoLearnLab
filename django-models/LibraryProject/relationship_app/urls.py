897a893d63250635f85344e4e27b3e940f94b061
Fraol <fraolm41@gmail.com>
2026-01-18 22:45:46 +0300

    fix urls.py file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,16 @@
+
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import list_books, LibraryDetailView, register_view
+from . import views  # import the whole views module
+
 
 urlpatterns = [
     # Books and Libraries
-    path('books/', list_books, name='list_books'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', views.list_books, name='list_books'),
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # Authentication
-    path('register/', register_view, name='register'),  # function-based view
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),  # class-based
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),  # class-based
+    path('register/', views.register_view, name='register'),  # now uses views.register_view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
