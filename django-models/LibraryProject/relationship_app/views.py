24b1d65c412463fd4fcc7d274f29f51f7ca42715
Fraol <fraolm41@gmail.com>
2026-01-18 22:33:42 +0300

    fix views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,18 @@
-from django.shortcuts import render
-
-# Create your views here.
+# LibraryProject/relationship_app/views.py
 
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Library, Book  # <-- MUST include Library
+from django.views.generic.detail import DetailView  # <-- MUST be exactly this
+from .models import Book, Library  # <-- MUST include Library
 
-
+# Function-based view: list all books
 def list_books(request):
-    books = Book.objects.all()  # <-- MUST be exactly this
+    books = Book.objects.all()  # <-- ALX literal check
     return render(request, 'relationship_app/list_books.html', {
         'books': books
     })
 
-
+# Class-based view: library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'  # <-- MUST match this exactly
-    context_object_name = 'library' 
+    template_name = 'relationship_app/library_detail.html'  # ALX literal check
+    context_object_name = 'library'  # ALX literal check
