8039cc90f54dbaba9d83a995837411f3c02c1e8d
Fraol <fraolm41@gmail.com>
2026-01-28 06:11:00 +0300

    forget to add generics in view.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
