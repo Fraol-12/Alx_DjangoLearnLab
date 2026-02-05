a1fa013432346ad4d9e74efbe41bcd11848b595e
Fraol <fraolm41@gmail.com>
2026-02-05 21:22:12 +0300

    fix minor error

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
 
 
 class BookListView(generics.ListAPIView):
