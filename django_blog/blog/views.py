2058fc6203c22e73dc069bfd28ded0a0448d3ae3
Fraol <fraolm41@gmail.com>
2026-02-12 12:29:24 +0300

    fix: add forms and allow update in profiles

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required 
 from django.contrib import messages
 from django.http import HttpResponse
+from .forms import UserUpdateForm
+
 
 def home(request):
     return render(request, 'blog/base.html')
@@ -28,4 +30,13 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'blog/profile.html') 
+    if request.method == 'POST':
+        form = UserUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfuly!')
+            return redirect('profile')
+    else:
+        form = UserUpdateForm(instance=request.user)    
+
+    return render(request, 'blog/profile.html', {'form': form})    
