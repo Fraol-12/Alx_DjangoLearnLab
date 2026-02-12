5b125c259dd1a9d7c72380a2b72436ed35e11a15
Fraol <fraolm41@gmail.com>
2026-02-12 12:13:58 +0300

    feat(auth): add registration, login, logout, and profile management with django built-in authentication views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from django.http import HttpResponse
+from . import views
+from django.contrib.auth import views as auth_views
+
 
 def home(request):
     return HttpResponse("Blog Home Page")
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+
+    path('login', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name='blog/Logout'), name='Logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('posts/', views.post_list, name='posts'),
+
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required 
+from django.contrib import messages
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'blog/base.html')
+
+def post_list(request):
+    return HttpResponse("post list page")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account Created Successfully!')
+            return redirect('login')
+        
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'blog/register.html', {'form':form})       
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html') 
