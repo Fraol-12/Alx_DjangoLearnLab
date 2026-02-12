fee3638189ca60496eb2e8a666ec9a64e2f29c3d
Fraol <fraolm41@gmail.com>
2026-02-12 12:44:29 +0300

    fix: forget to add trailing slashes for login and logout

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -10,10 +10,10 @@ def home(request):
 urlpatterns = [
     path('', views.home, name='home'),
 
-    path('login', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(template_name='blog/Logout.html'), name='Logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('posts/', views.post_list, name='posts'),
-
 ]
