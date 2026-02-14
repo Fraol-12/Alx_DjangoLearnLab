feff0b689e5a242fc27ac2aea15cded518945dee
Fraol <fraolm41@gmail.com>
2026-02-15 00:59:17 +0300

    feat(blog): add TagWidget to PostForm for proper tag input

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms 
 from django.contrib.auth.models import User 
 from .models import Post , Comment, Tag
-
+from taggit.forms import TagWidget
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
@@ -13,7 +13,10 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'content', 'tags']        
+        fields = ['title', 'content', 'tags'] 
+        widgets = {
+            'tags': TagWidget(attrs={'placeholder': 'Enter tags separated by commas'}),
+        }       
 
     def save(self, commit=True):
         instance = super().save(commit=False)    
